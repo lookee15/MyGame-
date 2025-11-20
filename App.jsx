139d3c6800993e80a734f20f0f467acd4d4f2ce0
Kabir <kadekanbi@algomau.ca>
2025-11-20 03:53:25 -0500

    Set up root component to render Game interface

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './Game';
+import './styles/Game.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
